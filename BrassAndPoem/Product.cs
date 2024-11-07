22d6cd067d46e854ab0cf29bf54cc175396f17b7
Daniel Randle <155481976+Sickle41@users.noreply.github.com>
2024-11-07 09:36:11 -0600

    Update Product.cs

--- BrassAndPoem/Product.cs
+++ BrassAndPoem/Product.cs
@@ -1,1 +1,6 @@
-//create your Product class here
+public class Product
+{
+    public string Name { get; set; }  // Name of the product
+    public decimal Price { get; set; }  // Price of the product
+    public int ProductTypeId { get; set; }  // Id of the product's type
+}
